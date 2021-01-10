47a4dabe5d820a4b96c0db7f68b839420d705a5f
Andrew Mitchell <mitchaj2@mcmaster.ca>
2021-01-10 17:18:42 -0500

    server backend should now work with multiple input documents

--- api/server.py
+++ api/server.py
@@ -1,7 +1,8 @@
 from flask import Flask, request
 from flask_cors import CORS
+import json
 from jamesMain import process
-from jamesClasses import jamesCorpus
+from jamesClasses import inputCorpus
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -9,14 +10,15 @@ cors = CORS(app)
 @app.route('/upload', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
+        corpus = inputCorpus()
         for x in range(int(request.form["fileCount"])):
-            if 'file' + str(x) not in request.files:
+            file = 'file' + str(x)
+            if file not in request.files:
                 return 'Error with attached files'
-        # This is just dummy code
-        # This will also need to be a loop like above
-        raw_data = request.files.get("file0").read()
-        response = {
-            "file0": str(raw_data)}
+            contents = request,fukes,get(file).read()
+            corpus.addDoc(file,contents)
+        results = process(corpus)
+        response = json.dumps(results)
         return response, 200
 
 if __name__ == '__main__':
