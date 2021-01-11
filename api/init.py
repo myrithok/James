c36a920a52aae3451f1a67a1ac08e21eab6c12a1
Andrew Mitchell <mitchaj2@mcmaster.ca>
2021-01-11 18:22:21 -0500

    Added comments to init, jamesConfig, and jamesTesting

--- api/init.py
+++ api/init.py
@@ -1,18 +1,29 @@
+#This script is used to prepare everything necessary for the James backend to run
+#The intention is that this script is run only once on setup, and then again
+#	only if something loaded by this file needs to change
+
+#Library imports
 import nltk
 import numpy as np
 import pickle
+#Project imports
 from jamesSA import buildSentimentModel
 import jamesConfig
 
+#The init method performs all necessary initialization
 def init():
+	#Set a seed, and load everything necessary from nltk
 	np.random.seed(2018)
 	nltk.download('wordnet')
 	nltk.download('twitter_samples')
 	nltk.download('punkt')
 	nltk.download('averaged_perceptron_tagger')
+	#Build the sentiment model using buildSentimentModel, imported from jamesSA
 	sentimentModel = buildSentimentModel()
+	#Save the sentiment model using pickle, to a filename imported from jamesConfig
 	f = open(jamesConfig.sentimentFilename(),"wb")
 	pickle.dump(sentimentModel,f)
 	f.close()
-
+	
+#Run init
 init()
--- api/jamesConfig.py
+++ api/jamesConfig.py
@@ -1,5 +1,11 @@
+#This file is meant to gather hard-coded names or values in one place
+#sentimentFilename is the name of the saved sentiment analysis model 
+#This is used to save and load the model by jamesSA
 def sentimentFilename():
 	return "jamesSentimentModel.pickle"
+#jamesChunkSize, jamesPasses, jamesIterations, and jamesEvalEvery are
+#	all parameters for building a topic model
+#They are used by jamesLDA
 def jamesChunkSize():
 	return 2000
 def jamesPasses():
