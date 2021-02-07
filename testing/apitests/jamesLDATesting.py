2caa39cec38d4048a2e3b496026dc8b1fee0a9d0
Andrew Mitchell <mitchaj2@mcmaster.ca>
2021-02-07 16:29:49 -0500

    Minor cleanup of testdata

--- testing/apitests/jamesLDATesting.py
+++ testing/apitests/jamesLDATesting.py
@@ -13,7 +13,7 @@ from api.jamesConfig import cfg
 
 # Method for loading and constructing a test corpus from a file in testdata
 def loadTestCorpus():
-    filename = os.path.join(os.path.dirname(os.path.dirname(__file__)),'testdata','testdata.txt')
+    filename = os.path.join(os.path.dirname(os.path.dirname(__file__)),'testdata','testdata1.txt')
     f = open(filename,'r')
     testData = f.read()
     f.close()
