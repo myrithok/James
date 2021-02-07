669b65d19b22a2da90c881e46fccc31de94a00c0
Esam Haris <36489659+eaharis@users.noreply.github.com>
2021-02-06 20:45:08 -0500

    Added additional test requests

--- testing/locustfile.py
+++ testing/locustfile.py
@@ -1,16 +1,26 @@
 from locust import HttpUser, task, between, tag
 
 class WebsiteUser(HttpUser):
-    wait_time = between(3, 5)
+    wait_time = between(2, 5)
 
     @tag('POST')
     @task(3)
-    def uploadPost(self):
+    def uploadMicro(self):
         test_request = {
             "fileCount": 1,
             "numTopics": 2
         }
         file = open('macro_micro.txt', 'r')
+        self.client.post("/upload", data=test_request, files={'file0': file})
+
+    @tag('POST')
+    @task(3)
+    def uploadNetflix(self):
+        test_request = {
+            "fileCount": 1,
+            "numTopics": 2
+        }
+        file = open('netflix_chips.txt', 'r')
         self.client.post("/upload", data=test_request, files={'file0': file})
 
     @tag('GET')
