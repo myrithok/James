694c1381d5dc2fc91fcec6d986266a5c2f72c05b
Andrew Mitchell <mitchaj2@mcmaster.ca>
2021-02-21 23:28:37 -0500

    Added bug test link to frontend

--- ui/src/components/ApplicationDescription/ApplicationDescription.js
+++ ui/src/components/ApplicationDescription/ApplicationDescription.js
@@ -1,11 +1,14 @@
 import React from "react";
-import { descriptionText } from "../../resources";
+import { descriptionText, bugText } from "../../resources";
 
 const ApplicationDescription = () => {
   return (
     <div className="description-container" data-testid="description-container">
-      <h4>James Description</h4>
+      <h4>Welcome to James</h4>
       <p>{descriptionText}</p>
+      <h4>Help up improve</h4>
+      <p>{bugText}</p>
+      <a href="https://github.com/myrithok/James/issues">here</a>
     </div>
   );
 };
