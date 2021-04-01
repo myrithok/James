d000c40d04804998c89af1584c5d13023d050788
Andrew Mitchell <mitchaj2@mcmaster.ca>
2021-03-31 22:40:53 -0400

    Finalized api testing

--- testing/apitests/jamesConfigTesting.py
+++ testing/apitests/jamesConfigTesting.py
@@ -18,18 +18,26 @@ class TestJamesConfig_cfg(unittest.TestCase):
     # Test that the cfg property topicwords exists, and is an int
     def test_topicwords_is_int(self):
         self.assertIsInstance(jamesConfig.cfg['topicwords'],int)
-    # Test that the cfg property mintokenlen exists, and is an int
-    def test_topicwords_is_int(self):
-        self.assertIsInstance(jamesConfig.cfg['mintokenlen'],int)
     # Test that the cfg property exsnum exists, and is an int
-    def test_topicwords_is_int(self):
+    def test_exsnum_is_int(self):
         self.assertIsInstance(jamesConfig.cfg['exsnum'],int)
+    # Test that the cfg property mintokenlen exists, and is an int
+    def test_mintokenlen_is_int(self):
+        self.assertIsInstance(jamesConfig.cfg['mintokenlen'],int)
+    # Test that the malletsettings propety exists, and is a dict
+    def test_malletsettings_is_dict(self):
+        self.assertIsInstance(jamesConfig.cfg['malletsettings'],dict)
+    # Test that mallet settings has a value for every requried setting, and that each of these is the correct type
+    def test_has_required_malletsettings(self):
+        requiredsettings = {'gamma_threshold':float, 'iterations':int, 'random_seed':int, 'minimum_probability':float}
+        for requiredsetting in requiredsettings:
+            self.assertIsInstance(jamesConfig.cfg['malletsettings'][requiredsetting],requiredsettings[requiredsetting])
+    # Test that the coherencetype propety exists, and is a str
+    def test_coherencetype_is_str(self):
+        self.assertIsInstance(jamesConfig.cfg['coherencetype'],str)
     # Test that the cfg property jdkversion exists, and is a str
     def test_jdkversion_is_str(self):
         self.assertIsInstance(jamesConfig.cfg['jdkversion'],str)
-    # Test that the cfg property satraining exists
-    def test_has_training_data(self):
-        self.assertIn('satraining',jamesConfig.cfg)
     # Test that the cfg property repo->mallet exists, and is a str
     def test_has_mallet_repo(self):
         self.assertIsInstance(jamesConfig.cfg['repo']['mallet'],str)
@@ -41,9 +49,19 @@ class TestJamesConfig_cfg(unittest.TestCase):
         self.assertIsNot(jamesConfig.cfg['path'],{})
     # Test that the cfg property path has a value for every required path, and that each of these is a str
     def test_has_required_paths(self):
-        requiredpaths = ['api','sapath','safile','malletpath','malletfile','malletlogging','tmp','antpath','antfile','antbin']
+        requiredpaths = ['api','malletpath','malletfile','malletlogging','tmp','antpath','antfile','antbin']
         for requiredpath in requiredpaths:
             self.assertIsInstance(jamesConfig.cfg['path'][requiredpath],str)
+        requiredpaths = ['pn','so',]
+        for requiredpath in requiredpaths:
+            self.assertIsInstance(jamesConfig.cfg['path'][requiredpath],list)
+        # Test that the host propety exists, and is a dict
+    def test_host_is_dict(self):
+        self.assertIsInstance(jamesConfig.cfg['host'],dict)
+    # Test that host has a value for every requried setting, and that each of these is the correct type
+    def test_has_required_host(self):
+        self.assertIsInstance(jamesConfig.cfg['host']['ip'],str)
+        self.assertIsInstance(jamesConfig.cfg['host']['port'],int)
 
 # Run the tests
 if __name__ == '__main__':
